eb38353afa6d4e7f0f03618a85a3fcb38b494ac5
Jessica Joseph <Jessicajoseph@nyu.edu>
2016-08-06 20:36:08 -0400

    pulling location and zooming in

---
+++ HumD/CameraViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CameraViewController.swift
+//  HumD
+//
+//  Created by Jessica Joseph on 8/6/16.
+//  Copyright © 2016 Jessica Joseph. All rights reserved.
+//
+
+import UIKit
+
+
+class CameraViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
