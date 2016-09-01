03cbf3c779814fd79e2492c0cd5f88d899e00450
Jessica Joseph <Jessicajoseph@nyu.edu>
2016-09-01 13:38:39 -0400

    add custom twitter button

--- HumD/TwitterViewController.swift
+++ HumD/TwitterViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 import TwitterKit
 
 class TwitterViewController: UIViewController {
-
+    
+    @IBOutlet weak var twtitterLogin: TWTRLogInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let logInButton = TWTRLogInButton { (session, error) in
+        twtitterLogin = TWTRLogInButton { (session, error) in
             if let unwrappedSession = session {
                 let alert = UIAlertController(title: "Logged In",
                                               message: "User \(unwrappedSession.userName) has logged in",
@@ -26,28 +28,20 @@ class TwitterViewController: UIViewController {
                 
                 if let userID = Twitter.sharedInstance().sessionStore.session()?.userID {
                     let client = TWTRAPIClient(userID: userID)
-                
                     
                     let parameter : [String : AnyObject] = ["screen_name" : (session?.userName)! , "count" : "200"]
                     let request = client.urlRequest(withMethod: "GET", url:"https://api.twitter.com/1.1/statuses/user_timeline.json" , parameters: parameter, error: nil)
  
-              var response : URLResponse?
-                let data = try! NSURLConnection.sendSynchronousRequest(request, returning: &response)
-                let arrayRep = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
-                    print("PRINTING THIS ARRAY TIMELINE \(arrayRep)")
-                
+                    // getting the response, and getting the timeline
+                    var response : URLResponse?
+                    let data = try! NSURLConnection.sendSynchronousRequest(request, returning: &response)
+                    let arrayRep = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
+                        print("PRINTING THIS ARRAY TIMELINE \(arrayRep)")
                 }
-                
-                
             } else {
                 NSLog("Login error: %@", error!.localizedDescription);
             }
         }
-        
-        // TODO: Change where the log in button is positioned in your view
-        logInButton.center = self.view.center
-        self.view.addSubview(logInButton)
-
     }
 
 }
